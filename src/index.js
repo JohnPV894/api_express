6ab11ab1dac13a0cd129b15aec6c80cb2d377f11
JohnPV894 <arenalessantiago1@gmail.com>
2025-01-22 23:43:00 +0100

    vercel 6

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
 });
+
+// Exporta la aplicación para que Vercel la maneje como una función.
+module.exports = app;
